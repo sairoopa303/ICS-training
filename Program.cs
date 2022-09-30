0022b876cf5dac370895526bdd56955ebc8634da
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-01 00:16:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Program2
+{
+    public static void Main()
+    {
+        int num;
+        Console.Write("\n\n");
+        Console.Write("Check whether given a number positive or negative:\n");
+        Console.Write("----------------------------------------");
+        Console.Write("\n\n");
+        Console.Write("Inout an integer : ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num >= 0)
+            Console.WriteLine("{0} is a positive number.\n", num);
+        elses
+            Console.Write("{0} is a negative number.\n", num);
+        Console.ReadLine();
+    }
+}
