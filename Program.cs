4b98242da3242e4913ad28d4ed50902657f3f81d
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 18:33:05 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-public class Program1
+public class Program2
 {
     public static void Main()
     {
-        int int1, int2;
+        int num;
         Console.Write("\n\n");
-        Console.Write("Check whether two integers are eual or not:\n");
+        Console.Write("Check whether given a number positive or negative:\n");
         Console.Write("----------------------------------------");
         Console.Write("\n\n");
-        Console.Write("Inout 1st number: ");
-        int1 = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Inout 2nd number: ");
-        int2 = Convert.ToInt32(Console.ReadLine());
-        if (int1 == int2)
-            Console.WriteLine("{0} and {1} are equal.\n", int1, int2);
-        else
-            Console.Write("{0} and {1} are not equal.\n", int1, int2);
+        Console.Write("Inout an integer : ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num >= 0)
+            Console.WriteLine("{0} is a positive number.\n", num);
+        elses
+            Console.Write("{0} is a negative number.\n", num);
         Console.ReadLine();
     }
 }
