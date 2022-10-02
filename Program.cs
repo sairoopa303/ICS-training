1e762716877c7590cbfc3eca45d19945150cdc95
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 18:42:25 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,52 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
-public class Assign2
+class program31
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        double rl, phy, che, ca, total;
-        double per;
-        string nm, div;
+        string str;
+        int l = 0;
 
-        Console.Write("\n\n");
-        Console.Write("Calculate the total, percentage and division to take marks of three subjects:\n");
-        Console.Write("-------------------------------------------------------------------------------");
-        Console.Write("\n\n");
+        Console.Write("\n\n find length of a string \n");
 
-
-        Console.Write("Input the Roll Number of the student :");
-        rl = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Input the Name of the Student :");
-        nm = Console.ReadLine();
-
-        Console.Write("Input  the marks of Physics : ");
-        phy = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Input  the marks of  Chemistry : ");
-        che = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Input  the marks of Computer Application : ");
-        ca = Convert.ToInt32(Console.ReadLine());
-
-        total = phy + che + ca;
-        per = total / 3.0;
-        if (per >= 60)
-            div = "First";
-        else
-            if (per < 60 && per >= 48)
-            div = "Second";
-        else
-                if (per < 48 && per >= 36)
-            div = "Pass";
-        else
-            div = "Fail";
-
-        Console.Write("\nRoll No : {0}\nName of Student : {1}\n", rl, nm);
-        Console.Write("Marks in Physics : {0}\nMarks in Chemistry : {1}\nMarks in Computer Application : {2}\n", phy, che, ca);
-        Console.Write("Total Marks = {0}\nPercentage = {1}\nDivision = {2}\n", total, per, div);
-
+        Console.Write("enter input string: ");
+        str = Console.ReadLine();
+        foreach (char chr in str)
+        {
+            l += 1;
+        }
+        Console.Write("length of string is {0}\n\n", l);
         Console.ReadLine();
     }
 }
