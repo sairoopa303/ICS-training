0d611f0e973d80e0edf0961e4c2fe93a370575e4
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 18:31:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Program1
+{
+    public static void Main()
+    {
+        int int1, int2;
+        Console.Write("\n\n");
+        Console.Write("Check whether two integers are eual or not:\n");
+        Console.Write("----------------------------------------");
+        Console.Write("\n\n");
+        Console.Write("Inout 1st number: ");
+        int1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Inout 2nd number: ");
+        int2 = Convert.ToInt32(Console.ReadLine());
+        if (int1 == int2)
+            Console.WriteLine("{0} and {1} are equal.\n", int1, int2);
+        else
+            Console.Write("{0} and {1} are not equal.\n", int1, int2);
+        Console.ReadLine();
+    }
+}
