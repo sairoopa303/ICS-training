9c3b3ed1fef2b6e7df0a39c8c0e4a8775c196ec6
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 18:38:18 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,29 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-class program5
+
+using System;
+
+class Assing2Avg
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        double fahrenheit;
-        double celsius = 6;
-        Console.WriteLine("Celsius: " + celsius);
-        fahrenheit = (celsius * 9) / 5 + 32;
-        Console.WriteLine("Fahrenheit: " + fahrenheit);
+        int[] arr = { 1, 2, 6, 2, 18 };
+
+        int i = 0;
+        int sum = 0;
+        float average = 0.0F;
+
+        for (i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        average = (float)sum / arr.Length;
+
+        Console.WriteLine("Average of Array elements: " + average);
         Console.ReadLine();
     }
 }
