c7d20635922f8d74dda8fb1baebcdeac1f8d4514
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 18:39:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System;
-
-class Assing2Avg
+public class MaxMin
 {
     public static void Main()
     {
-        int[] arr = { 1, 2, 6, 2, 18 };
-
-        int i = 0;
-        int sum = 0;
-        float average = 0.0F;
-
-        for (i = 0; i < arr.Length; i++)
+        int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+        int i, max, min, n;
+        n = 5;
+        max = arr[0];
+        min = arr[0];
+        for (i = 1; i < n; i++)
         {
-            sum += arr[i];
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
         }
-
-        average = (float)sum / arr.Length;
-
-        Console.WriteLine("Average of Array elements: " + average);
+        Console.Write("Maximum element = {0}\n", max);
+        Console.Write("Minimum element = {0}\n\n", min);
         Console.ReadLine();
     }
 }
+
+
