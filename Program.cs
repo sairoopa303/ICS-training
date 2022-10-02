5abbb1236495998093d7adb1908c7db400957a8e
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 18:34:21 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-public class Program2
+public class Program3
 {
     public static void Main()
     {
-        int num;
-        Console.Write("\n\n");
-        Console.Write("Check whether given a number positive or negative:\n");
-        Console.Write("----------------------------------------");
-        Console.Write("\n\n");
-        Console.Write("Inout an integer : ");
-        num = Convert.ToInt32(Console.ReadLine());
-        if (num >= 0)
-            Console.WriteLine("{0} is a positive number.\n", num);
-        elses
-            Console.Write("{0} is a negative number.\n", num);
+        int A, B;
+        char operation;
+        Console.Write("Inout first number: ");
+        A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Inout operation: ");
+        operation = Convert.ToChar(Console.ReadLine());
+        Console.Write("Inout second number: ");
+        B = Convert.ToInt32(Console.ReadLine());
+        if (operation == '+')
+            Console.WriteLine("{0} + {1} = {2}", A, B, A + B);
+        else
+            Console.Write("Wrong Character");
         Console.ReadLine();
     }
 }
