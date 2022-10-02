51bb76185dc9f02ab8f61517ae8c839caffcba28
sairoopa303 <114416155+sairoopa303@users.noreply.github.com>
2022-10-02 18:48:01 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,81 +1,78 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day3
+namespace Assign3_2_
 {
-
-    class Program
+    class Student
     {
-        public int AccountNum;
-        public string CustomerName;
-        string AccType;
-        char TransactionType;
-        float amount;
-        float balance;
+        int rollNum;
+        string name;
+        string year;
+        string branch;
+        string sem;
+        int[] mark = new int[5];
+        int Total;
+        int avg;
+        string result;
+        int Count = 0;
 
+        public Student(int rollNum, string name, string year, string branch, string sem)
+        {
+            this.rollNum = rollNum;
+            this.name = name;
+            this.year = year;
+            this.branch = branch;
+            this.sem = sem;
 
-        public static void Main(string[] args)
-        {
-            Program p1 = new Program(010, "Savings", "sairoopa");
-            p1.amount = 0.1f;
-            p1.balance = 20000.00f;
-            p1.ShowData();
-            Console.WriteLine("Enter transaction type 'D' for Deposit and 'W' for Withdrawl ");
-            p1.TransactionType = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("Enter the Amount");
-            p1.amount = Convert.ToSingle(Console.ReadLine());
-            p1.balance = p1.Ttype(p1.amount, p1.TransactionType);
-            Console.WriteLine("******************After Transaction*******************");
-            p1.ShowData();
-            Student s1 = new Student(1098, "sairoopa", "IInd year", "Computer Science", "3");
-            s1.Getmarks();
-            s1.DisplayResult();
-            s1.DisplayData();
-            Console.ReadLine();
         }
-        public Program(int num, string actype, string nme)
+        public void Getmarks()
         {
-            AccountNum = num;
-            AccType = actype;
-            CustomerName = nme;
-        }
-        public float Ttype(float amount, char Type)
-        {
-            if ('d' == Type)
+            Console.WriteLine("Enter the marks");
+            for (int i = 0; i < mark.Length; i++)
             {
-                return Credit(amount);
+                Console.WriteLine("enter the marks for subject:{0} ", i + 1);
+                mark[i] = Convert.ToInt32(Console.ReadLine());
+                if (mark[i] < 35)
+                {
+                    Count++;
+                }
+
+                Total = mark[i] + Total;
             }
-            else if ('w' == Type)
+            Console.WriteLine("Total of given marks : " + Total);
+        }
+        public void DisplayResult()
+        {
+            avg = Total / mark.Length;
+
+            if (Count > 0 && avg < 50)
             {
-                return Debit(amount);
+                result = "Fail";
             }
             else
             {
-                return 0;
+                result = "Pass";
             }
-        }
-        public float Credit(float deposit)
-        {
-            balance = balance + deposit;
-            return balance;
-        }
-        public float Debit(float Withdrawl)
-        {
-            balance = balance - Withdrawl;
-            return balance;
-        }
 
-        public void ShowData()
-        {
-            Console.WriteLine("********* Account Details**********");
-            Console.WriteLine("Account Number: " + AccountNum);
-            Console.WriteLine("Account Type: " + AccType);
-            Console.WriteLine("Balance: " + balance);
 
         }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("************student details*************");
+            Console.WriteLine("Roll Number: " + rollNum);
+            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("Year of Studying: " + year);
+            Console.WriteLine("Branch: " + branch);
+            Console.WriteLine("Semester: " + sem);
+            Console.WriteLine("Result is: " + result);
+            Console.ReadLine();
+        }
+
     }
-   
+}
+
+
